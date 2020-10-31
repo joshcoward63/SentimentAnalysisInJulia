c1fd29378edc1922dbdd4b84c43700fe6be6b0f5
Andrew Lee <andrewlee733@u.boisestate.edu>
2020-10-31 13:57:57 -0600

    Updated file structure and fixed svmmtrain input

--- bayes.jl
+++ bayes.jl
@@ -13,12 +13,6 @@ end
 # Functions
 # returns true if the given tweet is predicted to be positive
 function predict_positive(tweet, model)
-    # Variables
-    pos_total = model.pos_total
-    neg_total = model.neg_total
-    pos_occurences = model.pos_occurences
-    neg_occurences = model.neg_occurences
-
     # Helper functions
     # number of times word appears with negative sentiment
     neg_count(word) = haskey(neg_occurences, word) ? neg_occurences[word] : 0
@@ -33,6 +27,12 @@ function predict_positive(tweet, model)
     # probability sentiment is positive given a tweet
     prob_pos(tweet) = prod(prob_word_pos.(split(tweet))) * pos_total
 
+    # Variables
+    pos_total = model.pos_total
+    neg_total = model.neg_total
+    pos_occurences = model.pos_occurences
+    neg_occurences = model.neg_occurences
+
     # compare both probabilities and return true or false
     prob_pos(tweet) > prob_neg(tweet)
 end
@@ -45,7 +45,7 @@ const positive = 1
 # Functions
 is_pos_tweet(row) = parse(Int, string(row.class_label)) == positive
 function train(set)
-    # helper functions
+    # Helper functions
     add_neg_word(word) = !haskey(neg_occurences, word) ? neg_occurences[word] = 1 : neg_occurences[word] += 1
     add_pos_word(word) = !haskey(pos_occurences, word) ? pos_occurences[word] = 1 : pos_occurences[word] += 1
     neg_word(word) = (add_neg_word(word); neg_total += 1)
@@ -53,7 +53,7 @@ function train(set)
     train_word(word, pos) = pos ? pos_word(word) : neg_word(word)
     train_row(row) = train_word.(split(removenoise(string(row.tweet))), is_pos_tweet(row))
 
-    # initialize variables 
+    # Variables 
     pos_total = 0
     neg_total = 0
     pos_occurences = Dict{AbstractString,Int}()
